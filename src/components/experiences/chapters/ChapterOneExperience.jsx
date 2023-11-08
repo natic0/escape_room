89f4ea4afc3be325364358ec6c0d9c47daaacad3
Velyxius <dadawdasd@hotmail.com>
2023-11-07 23:57:29 -0500

    Updated settings to match World uppercase folder

--- src/components/experiences/chapters/ChapterOneExperience.jsx
+++ src/components/experiences/chapters/ChapterOneExperience.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Bedroom from "../../../world/rooms/Bedroom";
-import Lights from "../../../world/staging/Lights";
+import Bedroom from "../../../World/rooms/Bedroom";
+import Lights from "../../../World/staging/Lights";
 
 const ChapterOneExperience = () => {
   return (
