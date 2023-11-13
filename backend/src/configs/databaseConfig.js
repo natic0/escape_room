7dfb673bb0a10d5bdd172aaadcfeec80a597a382
Marcela Mazo <marcela.mazo@correounivalle.edu.co>
2023-11-12 23:07:25 -0500

    Backend-Credenciales
    Se añade una carpeta Backend con credenciales para inicio de sesión con google

---
+++ backend/src/configs/databaseConfig.js
@@ -0,0 +1,13 @@
+
+/*const { createClient } = require("@supabase/supabase-js");
+
+// Credenciales supabase
+const supabaseUrl = "https://nruyzulawmmelctpnjaw.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = {
+	supabase,
+};¨
+*/
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ProjectRoutes from "./routes/ProjectRoutes";
 import GameStartMessage from "./world/staging/StartGame"; // Importa el nuevo componente
 
+
 const App = () => {
   return (
     <>
       <ProjectRoutes />
       <GameStartMessage /> {/* Agrega el mensaje de inicio */}
+
     </>
   );
 };
