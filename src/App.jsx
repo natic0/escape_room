9183006a9beabe903bc9b4ceee552e6ec686bfc4
Marcela Mazo <marcela.mazo@correounivalle.edu.co>
2023-11-13 21:16:35 -0500

    New Component
    Bed component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 import ProjectRoutes from "./routes/ProjectRoutes";
 import GameStartMessage from "./world/staging/StartGame"; // Importa el nuevo componente
 
@@ -7,7 +9,8 @@ const App = () => {
     <>
       <ProjectRoutes />
       <GameStartMessage /> {/* Agrega el mensaje de inicio */}
-
+      <Login/>
+      <SignUp/>
     </>
   );
 };
